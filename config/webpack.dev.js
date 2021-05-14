f19d8e0749173448eccc7bcdf39daa0661fc8f45
ziming <guoaihua@geetest.com>
2021-05-14 21:48:42 +0800

    feat:add loader

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     mode: 'development',
     entry: {
@@ -7,8 +8,16 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '\.js$/',
+                test: '/\.js$/',
                 use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -17,5 +26,24 @@ module.exports = {
             '~': path.resolve(__dirname, '../src')
         },
         extensions: ['.js', '.json']
-    }
+    },
+    devServer: {
+        port: 8000,
+        hot: true,
+        open: true,
+        host: '0.0.0.0',
+        openPage: 'index.html',
+        contentBase: [
+            path.join(__dirname, "../static"),
+            path.join(__dirname, "../template"),
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            inject: 'head',
+            template: path.resolve(__dirname, '../template/index.html'),
+            filename: 'index.html'
+        })
+    ],
+    devtool: 'source-map'
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import '~/a.scss';
 export default function(){
     console.log(111);
+    console.log(a123132);
 }
+
+console.log(111);
+console.log(221231);
