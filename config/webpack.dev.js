ee04983e02066b46974b68f11f139059dcc3259b
ziming <guoaihua@geetest.com>
2021-05-25 22:11:17 +0800

    feat:  add cache loader

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const smp = new SpeedMeasurePlugin();
+const threadLoader = require('thread-loader');
+
+threadLoader.warmup({
+  // pool options, like passed to loader options
+  // must match loader options to boot the correct pool
+}, [
+  // modules to load
+  // can be any module, i. e.
+  'cache-loader',
+  'babel-loader',
+  'sass-loader',
+]);
+
+
 module.exports = {
     mode: 'development',
     entry: {
@@ -9,14 +27,31 @@ module.exports = {
         rules: [
             {
                 test: '/\.js$/',
-                use: 'babel-loader'
+                include: '/src',
+                use: 
+                [
+                    'thread-loader',
+                    'cache-loader',
+                    'babel-loader'
+                ]
             },
             {
                 test: /\.scss$/,
                 use:[
-                    'style-loader',
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
                     'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options:{
+                            name: '[name].[ext]'
+                        }
+                    }
                 ]
             }
         ]
@@ -30,7 +65,7 @@ module.exports = {
     devServer: {
         port: 8000,
         hot: true,
-        open: true,
+        open: false,
         host: '0.0.0.0',
         openPage: 'index.html',
         contentBase: [
@@ -39,6 +74,7 @@ module.exports = {
         ]
     },
     plugins:[
+        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             inject: 'head',
             template: path.resolve(__dirname, '../template/index.html'),
